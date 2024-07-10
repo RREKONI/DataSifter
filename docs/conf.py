cff3b7ddb9b544399376ec5d2fdf76928c1acaf5
Rekoni <nilka.gacha@gmail.com>
2024-07-10 11:08:39 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,5 +3,5 @@ author = "Rekoni"
 copyright = "2024, Rekoni"
 version = "0.0.5.1"
 release = "0.0.5.1"
-html_static_path = "_static"
-html_css_files = "default.css"
+html_static_path = ["_static"]
+html_css_files = ["default.css"]
